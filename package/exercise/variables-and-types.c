cabfa4fc9f8f70853391478c41b7cc2dd7aa3924
Vinicius José <viniciusjosedev@gmail.com>
2025-03-28 20:30:44 -0300

    feat: add exercise variables-and-types.c

---
+++ package/exercise/variables-and-types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int var1 = 2;
+	float var2 = 2.1;
+	char var3 = 't';
+	char *var4 = "test";
+	
+	printf("var1 %i\n", var1);
+	printf("var1 %f\n", var2);
+	printf("var1 %c\n", var3);
+	printf("var1 %s\n", var4);
+
+	return 0;
+}
