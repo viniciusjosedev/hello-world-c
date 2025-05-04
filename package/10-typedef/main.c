3efa402beaccd78910cef68000f511e045f38cb7
Vinicius José <viniciusjosedev@gmail.com>
2025-05-04 16:14:37 -0300

    feat: change topics

--- package/10-typedef/main.c
+++ package/10-typedef/main.c
@@ -2,38 +2,39 @@
 
 typedef int new_int;
 
-struct new_struct {
+struct new_struct
+{
 	char name[1024];
 };
 
 typedef struct new_struct new_struct_def;
 
-typedef struct new_2_struct {
+typedef struct new_2_struct
+{
 	char name[1024];
 } new_2_struct_def;
 
-typedef struct {
+typedef struct
+{
 	char name[1024];
 } new_3_struct_def;
-
 
 typedef int *intp;
 
 typedef int five_intp[];
 
-int main() {
+int main()
+{
 	int i = 10;
 
-	new_struct_def new = {.name="test"};
-	new_2_struct_def new2 = {.name="test"};
-	new_3_struct_def new3 = {.name="test"};
-	
+	new_struct_def new = {.name = "test"};
+	new_2_struct_def new2 = {.name = "test"};
+	new_3_struct_def new3 = {.name = "test"};
+
 	printf("%s", new3.name);
 
 	intp p = &i;
 	five_intp array = {0};
 
 	return 0;
-
 }
-
